3296365e37129b94fa2ee988565de20144393ffa
MohamadNafrinShafni <nafrinn03@gmail.com>
2025-05-22 00:01:18 +0530

    Created Mar12 Folder with js files

---
+++ Mar12/One.m
@@ -0,0 +1,21 @@
+num = 4;
+
+if num>0
+    disp('The number is positive')
+else
+    disp('the number is negative')
+end    
+
+num = 10;
+
+if mod(num,2)==0
+    disp('The number is even')
+else
+    disp('The number is odd')
+end
+
+i = 1;
+while(i<6)
+    disp(i)
+    i= i+1;
+end
