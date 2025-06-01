05c114ead886cc783fbddb0f0d435566dfc67a6f
MohamadNafrinShafni <nafrinn03@gmail.com>
2025-06-01 21:32:47 +0530

    add May26 Folders with js Files

---
+++ May26/May26/insertionsort.m
@@ -0,0 +1,17 @@
+arr = [64, 34, 25, 12, 22, 11, 90];
+    n = length(arr);
+    for i = 2:n
+        key = arr(i);
+        j = i - 1;
+        % Move elements that are greater than key one position ahead
+        while j > 0 && arr(j) > key
+            arr(j + 1) = arr(j);
+            j = j - 1;
+        end
+        arr(j + 1) = key;
+    end
+   
+
+
+disp('Sorted array:');
+disp(arr);
